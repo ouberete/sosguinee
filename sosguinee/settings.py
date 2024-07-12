33744bbe458144710037a6be8624a9171b765501
Oumar BERETE <oumberete09@gmail.com>
2024-07-12 15:37:21 +0000

    second

--- sosguinee/settings.py
+++ sosguinee/settings.py
@@ -86,6 +86,16 @@ WSGI_APPLICATION = 'sosguinee.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 # Database MongoDB for SOS Guineense
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+""" 
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
@@ -96,15 +106,6 @@ DATABASES = {
         }
     }
 }
-
-""" 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-} 
-
 
 
 USERNAME = os.getenv("USERNAME")
