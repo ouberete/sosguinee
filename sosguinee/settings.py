c6b5c387c1718d6ca9621356a39e7b2997349fe6
Oumar BERETE <oumberete09@gmail.com>
2025-11-05 09:53:30 +0000

    chore: setting v

--- sosguinee/settings.py
+++ sosguinee/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-nqbfqt50fhyj158=c)nmfiw6_-8sv9ffzd#l+k=ojy-a=o+hav"
 
-DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost").split(",")
+DEBUG = True
+ALLOWED_HOSTS = ['*']
 
 # --- STATIC & MEDIA ---
 STATIC_URL = "/static/"
