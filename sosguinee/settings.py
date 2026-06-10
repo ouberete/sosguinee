5a555f71a40e310ab7382edd97c3a404f631fea6
ouberete <oumberete09@gmail.com>
2026-06-10 12:24:55 +0000

    render yaml

--- sosguinee/settings.py
+++ sosguinee/settings.py
@@ -103,6 +103,7 @@ SOCIALACCOUNT_PROVIDERS = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
